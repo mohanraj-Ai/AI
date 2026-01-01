6d2dd5a517c811c0a34ff8ca3dfbf0540b063053
Mohan Raj <raj07944@gmail.com>
2026-01-01 16:29:21 +0530

    Add files via upload

---
+++ python/HANDS ON/subfields.py
@@ -0,0 +1,11 @@
+class subfieldsInAI():
+    def subfields():
+        print("subfields in AI are:")
+        print("machine learning")
+        print("neural network")
+        print("vision")
+        print("Robotics")
+        print("speech processing")
+        print("Natural language processing")
+        
+        
